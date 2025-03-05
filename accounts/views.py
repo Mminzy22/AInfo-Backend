450074459946da76861f96c1c73f1578a1f7ea29
WIN-JSOA1D9C5C1\aw702 <aw7022@gmail.com>
2025-03-05 15:07:16 +0900

    ✨ Feature: 회원가입 API 뷰 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
-# from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions
 
-# Create your views here.
+from .serializers import SginupSerializer
+
+User = get_user_model()
+
+
+# 회원가입 (POST /api/v1/accounts/signup/)
+class SginupView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = SginupSerializer
+    permission_classes = [permissions.AllowAny]  # 누구나 회원가입 가능
