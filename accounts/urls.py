3b9fd092d4d8327e1990ce5186b615b97bac2a11
WIN-JSOA1D9C5C1\aw702 <aw7022@gmail.com>
2025-03-05 15:14:48 +0900

    ✨ Feature: 회원가입 API 뷰에 대한 URL 경로 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,9 @@
+from django.urls import path
+
+from .views import SginupView
+
 app_name = "accounts"
 
-urlpatterns = []
+urlpatterns = [
+    path("signup/", SginupView.as_view(), name="signup"),
+]
