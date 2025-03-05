9b5bdd97f85bd36e4a9d8c1852c7e5c7737f4a5e
WIN-JSOA1D9C5C1\aw702 <aw7022@gmail.com>
2025-03-05 11:57:34 +0900

    ✨ Feature: 커스텀 유저 모델을 위한 UserManager 추가

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
-# from django.db import models
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """일반 사용자 생성"""
+        if not email:
+            raise ValueError("이메일을 입력해야 합니다.")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """슈퍼유저 생성"""
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
