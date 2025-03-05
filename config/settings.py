77c94cb25c206bf9107682463e8953678225c946
WIN-JSOA1D9C5C1\aw702 <aw7022@gmail.com>
2025-03-05 10:42:49 +0900

    🔧 Setup: DATABASES 설정을 DEBUG 모드에 따라 변경

--- config/settings.py
+++ config/settings.py
@@ -97,10 +97,14 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': (
+        {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+        if DEBUG
+        else env.db("DATABASE_URL")
+    )
 }
 
 
