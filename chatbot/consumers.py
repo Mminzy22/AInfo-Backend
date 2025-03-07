8e7a17fc7d2d3d2236d0f27ccbb8f52d992dc650
jongyeol2 <114922306+jongyeol2@users.noreply.github.com>
2025-03-07 21:46:13 +0900

    ✨ Feat: 컨슈머 클래스에 receive 구현
    Ref: #31

--- chatbot/consumers.py
+++ chatbot/consumers.py
@@ -1,4 +1,8 @@
+import json
+
 from channels.generic.websocket import AsyncWebsocketConsumer
+
+from .utils import get_chatbot_response
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -7,3 +11,16 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def disconnect(self, close_code):
         pass
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        user_message = data["message"]
+
+        llm_response = await self.llm_response(user_message)
+
+        await self.send(text_data=json.dumps({"response": llm_response}))
+
+    async def llm_response(self, user_message):
+        llm_response = get_chatbot_response(user_message)
+        response = llm_response.get("response")
+        return response
