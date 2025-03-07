971589c93da07fde99e5b865c3a5218d2aa9ea65
jongyeol2 <114922306+jongyeol2@users.noreply.github.com>
2025-03-07 21:39:38 +0900

    ✨ Feat: 컨슈머 클래스에 disconnect 추가
    Ref: #31

--- chatbot/consumers.py
+++ chatbot/consumers.py
@@ -4,3 +4,6 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
+
+    async def disconnect(self, close_code):
+        pass
