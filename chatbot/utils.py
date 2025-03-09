414bf5d82587a5d2bb1f30cc6d3c0a3ecd4e5950
JaceJung-dev <jacejung.dev@gmail.com>
2025-03-09 14:08:19 +0900

    ✨ Feat: 비동기 스트리밍 지원을 위한 챗봇 모델 개선
    - 기존의 `get_chatbot_response()` 함수를 async generator로 변경해서 스트리밍 가능하게 변경
    - `ChatOpenAI`에 `streaming=True` 파라미터 추가
    - `astream()`을 사용하여 순차적인 응답 전송

--- chatbot/utils.py
+++ chatbot/utils.py
@@ -10,10 +10,16 @@ vector_retriever = VectorRetriever()
 retriever = vector_retriever.get_retriever()
 
 
-def get_chatbot_response(user_message):
+async def get_chatbot_response(user_message):
     """
+    비동기 스트리밍 방식 챗봇 응답을 생성할 수 있는 함수
+
     - 사용자의 입력을 받아 LLM을 실행하고, 단일 응답만 반환 (싱글턴, 대화 기록 저장 X)
     - temperature=0.3: 창의성보다 정확도에 중점을 둠. (추후 조정 가능)
+
+    변동사항 (03/09/25):
+    - async, yield를 사용해 비동기 작업을 할 수 있도록 변경
+    - LLM 응답을 invoke() 방식이 아닌 astream() 비동기 스트리밍 방식으로 변경
     """
 
     # 사용자 질문을 기반으로 문서 검색
@@ -33,7 +39,8 @@ def get_chatbot_response(user_message):
         retrieved_context = "정부의 지원 정책"
 
     # llm 실행 (CHATBOT_PROMPT는 utils1.py에서 import)
-    llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0.3)
+    # streaming 을 위한 streaming 파라미터 조정
+    llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0.3, streaming=True)
 
     prompt = CHATBOT_PROMPT
 
@@ -44,9 +51,5 @@ def get_chatbot_response(user_message):
         | StrOutputParser()
     )
 
-    response = rag_chain.invoke(user_message)
-
-    return {
-        "response": response,
-        "retrieved_context": retrieved_context,  # RAG에서 가져온 문서 같이 출력되도록 함(확인용)
-    }
+    async for chunk in rag_chain.astream(user_message):
+        yield chunk
