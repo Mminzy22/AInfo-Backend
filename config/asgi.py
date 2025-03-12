2e45df1c6e3ac452350d2373f81859b697e75f6a
JaceJung-dev <jacejung.dev@gmail.com>
2025-03-12 15:34:10 +0900

    🐛 Fix: Django가 완전히 로드되지 전에 모듈을 불러오는 오류 수정
    - `get_asgi_application`를 실행하기 전에 장고가 완전히 로드되지 않은 문제 수정
    - `import django` `django.setup()`을 통해 django를 먼저 로드하고 이후 모듈을 로드 할 수 있도록 수정

--- config/asgi.py
+++ config/asgi.py
@@ -1,12 +1,15 @@
 import os
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
+import django  # noqa: E402
 
-from chatbot.middleware import JWTAuthMiddleware
-from chatbot.routing import websocket_urlpatterns
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")  # noqa: E402
+django.setup()  # noqa: E402
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+from channels.routing import ProtocolTypeRouter, URLRouter  # noqa: E402
+from django.core.asgi import get_asgi_application  # noqa: E402
+
+from chatbot.middleware import JWTAuthMiddleware  # noqa: E402
+from chatbot.routing import websocket_urlpatterns  # noqa: E402
 
 application = ProtocolTypeRouter(
     {
