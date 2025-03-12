8bb16c32d21f9e5de28dfb7cfd0984410469bf75
Mminzy22 <aw7022@gmail.com>
2025-03-12 17:37:37 +0900

    ♻️ Refactor: 모델 임포트 순서 변경

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import CurrentStatus, EducationLevel, Interest, SubRegion, User, Region
+from .models import CurrentStatus, EducationLevel, Interest, Region, SubRegion, User
 
 
 @admin.register(CurrentStatus)
