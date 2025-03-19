89e551be49a77f516acb13a59aaf9675cdbb8c2f
Mminzy22 <aw7022@gmail.com>
2025-03-20 01:29:30 +0900

    ♻️ Refactor: 뉴스 검색 실행 모듈의 임포트 경로 수정

--- chatbot/langchain_flow/run.py
+++ chatbot/langchain_flow/run.py
@@ -1,6 +1,6 @@
 """LangChain 실행 진입점 (분기별 에이전트 실행 연결 예정)."""
 
 # LangChain 뉴스 검색 에이전트
-# from .chains.news_search_executor import news_search_executor
+# from .chains.news_search_executor import get_news_search_executor
 
 # TODO: 향후 분기 조건 추가 예정
