1a989bb8c94ec114ad5043bdbeb1babd6eceb8c5
JaceJung-dev <jacejung.dev@gmail.com>
2025-03-20 00:49:41 +0900

    ✨ Feat: 사용자별 대화 기록을 반영하는 멀티턴 기능 추가
    - - `user_id`를 이용해 사용자별 대화 히스토리를 관리
    - `ChatHistoryManager`를 이용해 대화기록을 관리
    - `get_memory_manager()`를 이용해 사용자별 메모리를 불러와, `chat_history`를 체인에 포함하여 멀티턴 구현
    - `memory_manager.save_context()`로 사용자 입력과 챗봇 응답을 메모리에 저장

--- chatbot/utils.py
+++ chatbot/utils.py
@@ -2,6 +2,7 @@ from langchain.schema.runnable import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 
+from .memory import ChatHistoryManager
 from .prompt import CHATBOT_PROMPT
 from .retriever import VectorRetriever
 
@@ -11,16 +12,26 @@ vector_retriever = VectorRetriever()
 retriever = vector_retriever.get_multi_query_retriever()
 
 
-async def get_chatbot_response(user_message):
+async def get_chatbot_response(user_message, user_id):
     """
     비동기 스트리밍 방식 챗봇 응답을 생성할 수 있는 함수
 
-    - 사용자의 입력을 받아 LLM을 실행하고, 단일 응답만 반환 (싱글턴, 대화 기록 저장 X)
+    이 함수는 LangChain의 활용해서 검색 증강 생성(RAG) 방식의 챗봇 응답을 생성합니다.
+    비동기 스트리밍으로 응답으로 실시간으로 반환하고, 대화 기록을 활용해 대화의 흐름을 이어나갈 수 있습니다.
+
+    주요 기능:
+    - 사용자의 입력을 받아 LLM을 실행하고, 비동기 스트리밍 방식으로 응답 바노한
+    - 검색된 문서를 기반으로 하는 답변 제공
+    - `ChatHistoryManager`를 통해 대화 내용을 관리할 수 있음
     - temperature=0.3: 창의성보다 정확도에 중점을 둠. (추후 조정 가능)
+
 
     변동사항 (03/09/25):
     - async, yield를 사용해 비동기 작업을 할 수 있도록 변경
     - LLM 응답을 invoke() 방식이 아닌 astream() 비동기 스트리밍 방식으로 변경
+
+    변동사항 (03/19/25)
+    - `ChatHistoryManager`를 통해 대화 내용 관리 및 멀티턴 기능 추가
     """
 
     # 사용자 질문을 기반으로 문서 검색
@@ -43,14 +54,27 @@ async def get_chatbot_response(user_message):
     # streaming 을 위한 streaming 파라미터 조정
     llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0.3, streaming=True)
 
+    chat_manager = ChatHistoryManager(user_id, llm)
+    memory_manager = chat_manager.get_memory_manager()
+    memory = memory_manager.load_memory_variables({})
+
     prompt = CHATBOT_PROMPT
 
     rag_chain = (
-        {"context": lambda _: retrieved_context, "question": RunnablePassthrough()}
+        {
+            "context": lambda _: retrieved_context,
+            "question": RunnablePassthrough(),
+            "chat_history": lambda _: memory.get("chat_history", []),
+        }
         | prompt
         | llm
         | StrOutputParser()
     )
 
+    output_response = ""
+
     async for chunk in rag_chain.astream(user_message):
+        output_response += chunk
         yield chunk
+
+    memory_manager.save_context({"human": user_message}, {"ai": output_response})
