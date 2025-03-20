2045d8de779c542097b509fff18a7c1ab1b4846a
cheachea <heekyung4105@gmail.com>
2025-03-20 13:11:12 +0900

    ✨ Feat: chatroom model 구현

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,3 +1,12 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+from accounts.models import User
+
+
+class ChatRoom(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, default="새 채팅")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
