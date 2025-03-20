99d64fa05746abf227b5cd569d69801a42349243
JuyoungYang <juyoung.yang11@gmail.com>
2025-03-20 13:50:54 +0900

    ✨ Feat: 공통 유틸리티 모듈 및 환경 설정 추가

---
+++ vector_store/common.py
@@ -0,0 +1,27 @@
+"""
+공통 유틸리티 모듈 (common.py)
+- ChromaDB 및 임베딩 관련 공통 함수 제공
+"""
+
+import os
+import traceback
+import django
+import environ
+from django.conf import settings
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+from tqdm import tqdm
+import logging
+
+# 환경 변수 및 Django 설정
+env = environ.Env()
+environ.Env.read_env()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
+
+# 설정 값 로드
+CHROMA_DB_DIR = settings.CHROMA_DB_DIR
+OPENAI_API_KEY = env("OPENAI_API_KEY")
+
+# ChromaDB 로깅 레벨 설정
+logging.getLogger("chromadb").setLevel(logging.ERROR)
