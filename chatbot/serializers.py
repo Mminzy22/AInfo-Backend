5e8b2083c2de3c74902349d1b0768015c664d5c7
cheachea <heekyung4105@gmail.com>
2025-03-20 13:17:33 +0900

    ✨ Feat: chatroom, chatlog serializer 구현

--- chatbot/serializers.py
+++ chatbot/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from .models import ChatLog, ChatRoom
+
 
 class ChatbotSerializer(serializers.Serializer):
     """
@@ -7,3 +9,30 @@ class ChatbotSerializer(serializers.Serializer):
     """
 
     message = serializers.CharField(max_length=500, required=True)
+
+
+class ChatLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChatLog
+        fields = "__all__"
+
+    def validate_role(self, value):
+        """role 값이 'user' 또는 'bot'만 가능"""
+        if value not in ["user", "bot"]:
+            raise serializers.ValidationError("role은 'user' 또는 'bot'만 가능합니다.")
+        return value
+
+
+class ChatRoomSerializer(serializers.ModelSerializer):
+    chatlogs = ChatLogSerializer(many=True, read_only=True)
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = ChatRoom
+        fields = "__all__"
+
+    def validate_title(self, value):
+        """채팅방 제목에 공백만 넣는것 방지"""
+        if not value.strip():
+            raise serializers.ValidationError("채팅방 제목을 입력하세요.")
+        return value
