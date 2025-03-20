2af1b44b00cb7739c603237ed57acbd2d4b4c6cc
cheachea <heekyung4105@gmail.com>
2025-03-20 13:50:55 +0900

    ✨ Feat: WebSocket과 특정 채팅방 연결

--- chatbot/consumers.py
+++ chatbot/consumers.py
@@ -1,10 +1,10 @@
 import json
 
-from asgiref.sync import sync_to_async
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 from accounts.models import User
-
+from .models import ChatRoom
 from .memory import ChatHistoryManager
 from .serializers import ChatbotSerializer
 from .utils import get_chatbot_response
@@ -23,12 +23,17 @@ class ChatConsumer(AsyncWebsocketConsumer):
     - `is_streaming=True`로 스트리밍 중임을 같이 알림
     - 생성이 끝나면 완전한 메시지와 'is_streaming=False`도 같이 보내 스트리밍이 끝남을 알림
     - 인증되지 않은 사용자는 메시지를 전송할 수 없으며, 에러 메시지를 반환
+    **변동사항(3/20)**
+    - WebSocket 연결 시 chatroom_id를 확인하고, 인증된 사용자가 만든 chatroom만 사용 가능
 
     매서드(Method)
     - connect(): WebSocket 연결을 초기화하고 사용자 인증을 수행함.
     - disconnect(close_code): Websocket 연결 종료
     - receive(text_data): 클라이언트의 메시지를 받아 Chatbot에 전달
     - get_user(): user_id를 기반으로 데이터베이스에서 사용자 정보 조회
+    **변동사항(3/20)**
+    - get_chatroom() : chatroom_id를 기반으로 데이터베이스에서 chatroom 정보 조회
+
     """
 
     async def connect(self):
@@ -39,12 +44,30 @@ class ChatConsumer(AsyncWebsocketConsumer):
             await self.close()
             return
 
+        # chatroom pk 확인
+        self.room_id = self.scope.get("url_route", {}).get("kwargs", {}).get("room_id")
+        if not self.room_id:
+            await self.close()
+            return
+
+        self.room_group_name = f"chatroom_{self.room_id}"
+
+        # chatroom_id DB조회
+        self.chatroom = await self.get_chatroom(self.room_id)
+        if not self.chatroom:
+            await self.close()
+            return
+
+        # WebSocket 그룹 추가
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
         if self.is_authenticated:
             chat_history_manager = ChatHistoryManager(self.user_id, model=None)
             chat_history_manager.clear_history()
+
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data):
         if not self.is_authenticated:
@@ -94,8 +117,16 @@ class ChatConsumer(AsyncWebsocketConsumer):
             )
         )
 
-    @sync_to_async
+    @database_sync_to_async
     def get_user(self):
         if self.user_id:
             return User.objects.filter(id=self.user_id).first()
         return None
+
+    @database_sync_to_async
+    def get_chatroom(self, room_id):
+        """DB에서 room_id에 해당하는 채팅방 가져오기"""
+        try:
+            return ChatRoom.objects.get(id=room_id)
+        except ChatRoom.DoesNotExist:
+            return None
