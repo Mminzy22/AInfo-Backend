43cf9caf73f25232b29d91fc3b9929dfd0601fe5
cheachea <heekyung4105@gmail.com>
2025-03-20 13:24:39 +0900

    ✨ Feat: chatroom 생성, 조회, 삭제, 수정 api 구현

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,3 +1,16 @@
-app_name = "chatbot"
+from django.urls import path
 
-urlpatterns = []
+from .views import (
+    ChatRoomDetailDeleteUpdateView,
+    ChatRoomListCreateView,
+)
+
+app_name = "chatbot"
+urlpatterns = [
+    path("room/", ChatRoomListCreateView.as_view(), name="chatroom-list-create"),
+    path(
+        "room/<int:pk>/",
+        ChatRoomDetailDeleteUpdateView.as_view(),
+        name="chatroom-detail-delete-patch",
+    ),
+]
