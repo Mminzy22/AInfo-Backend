fc11e05901e7b8d0297853dfc73bf9583d07c435
JuyoungYang <juyoung.yang11@gmail.com>
2025-03-20 13:49:45 +0900

    ✨ Feat: 모든 ChromaDB 컬렉션 삭제 함수 추가

--- vector_store/load_data.py
+++ vector_store/load_data.py
@@ -14,3 +14,28 @@ from .load_employment_data import process_and_store_employment_data
 from .load_gov24_data import process_and_store_gov24_data
 from .load_pdf_data import process_and_store_pdf_data
 from .load_youth_policy_data import process_and_store_youth_policy_data
+
+
+def delete_all_collections():
+    """
+    CHROMA_DB_DIR 내 모든 컬렉션 폴더를 삭제
+    """
+    chroma_dir = settings.CHROMA_DB_DIR
+
+    if not os.path.exists(chroma_dir):
+        tqdm.write("Chroma 디렉토리가 존재하지 않습니다.")
+        return
+
+    collections = [
+        f for f in os.listdir(chroma_dir) if os.path.isdir(os.path.join(chroma_dir, f))
+    ]
+
+    if collections:
+        tqdm.write("\n=== 모든 컬렉션 삭제 시작 ===")
+        for collection in collections:
+            path = os.path.join(chroma_dir, collection)
+            shutil.rmtree(path)
+            tqdm.write(f"'{collection}' 폴더가 삭제되었습니다.")
+        tqdm.write("=== 모든 컬렉션 삭제 완료 ===\n")
+    else:
+        tqdm.write("삭제할 컬렉션 폴더가 없습니다.")
