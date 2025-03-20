5de8f05079867305babc2a46a44deba2e8b3837d
JuyoungYang <juyoung.yang11@gmail.com>
2025-03-20 13:46:57 +0900

    ✨ Feat: 청년정책 API 로더 모듈 및 설정 추가

---
+++ vector_store/load_youth_policy_data.py
@@ -0,0 +1,33 @@
+"""
+청년정책 API 데이터 로더
+API에서 청년정책 데이터를 로드하여 ChromaDB에 저장
+"""
+
+import math
+import os
+import time
+import traceback
+
+import django
+import requests
+from django.conf import settings
+from langchain.schema import Document
+from tqdm import tqdm
+
+from .common import (
+    get_chroma_collection,
+    get_embeddings,
+    prepare_metadata_for_chroma,
+    save_documents_with_progress,
+    sanitize_metadata,
+)
+
+# Django 환경설정
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
+
+# API 설정
+YOUTH_API_BASE_URL = "https://www.youthcenter.go.kr/go/ythip/getPlcy"
+YOUTH_POLICY_API_KEY = settings.YOUTH_POLICY_API_KEY
+PAGE_SIZE = 10
+API_RATE_LIMIT_DELAY = 1
