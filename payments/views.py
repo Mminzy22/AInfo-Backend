24d753c943c4409e008a45199e426f92a21d3e49
jongyeol2 <114922306+jongyeol2@users.noreply.github.com>
2025-03-26 04:22:39 +0900

    ✨ Feat: 결제관련 로직 수정
    - Webhook 데이터 DB저장
    - 결제검증 요청 로직 개선

--- payments/views.py
+++ payments/views.py
@@ -6,6 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .models import Payment
 
 
 class PayVerify(APIView):
@@ -13,11 +14,16 @@ class PayVerify(APIView):
     def post(self, request):
         user = request.user
         payment_id = request.data.get("payment_id")
-        print("----" * 18)
-        print(f"user : {user}")
-        print(f"payment_id : {payment_id}")
-        print("----" * 18)
-        print(f"request : {request}")
+
+        payment = Payment.objects.filter(payment_id=payment_id).first()
+        
+        if not payment:
+            return Response({"error": "Invalid payment_id"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        payment.user = user
+        payment.save()
+        
+        # 유저 DB 에서 크레딧 증가시키는 로직 추가예정
 
         return Response(
             {"message": "결제 상태 업데이트 완료"}, status=status.HTTP_200_OK
@@ -28,31 +34,13 @@ class PayVerify(APIView):
 def payment_webhook(request):  # 추후에 비동기처리 고려
     if request.method == "POST":  # 웹훅 요청은 보통 POST 방식
 
-        try:
-            data = json.loads(request.body)  # 요청의 JSON 데이터를 파싱
-            payment_status = data.get("status")  # 결제 상태 (예: success, failed)
-            payment_id = data.get("payment_id")  # 결제 ID
+        data = json.loads(request.body)  # 요청의 JSON 데이터를 파싱
+        payment_status = data.get("status")  # 결제 상태 (예: Paid, Ready)
+        payment_id = data.get("payment_id")  # 결제 ID
 
-            print("----" * 18)
-            print(f"request : {request}")
-            print("----" * 18)
-            print(f"request.data : {request.data}")
-            print(f"request.type : {request.type}")
-            print(f"request.timestamp : {request.timestamp}")
-            print("----" * 18)
-            print(f"request.payment_id : {request.payment_id}")
-            print(f"request.tx_id : {request.tx_id}")
-            print(f"request.status : {request.status}")
-            header = request.headers
-            print(f"header : {header}")
 
-            # 결제가 성공했을 경우
-            if payment_status == "success":
-                print(f"✅ 결제 성공! ID: {payment_id}")
-                # TODO: 결제 정보를 데이터베이스에 저장하는 로직 추가
-
-            return JsonResponse({"message": "Webhook received"}, status=200)
-        except json.JSONDecodeError:
-            return JsonResponse({"error": "Invalid JSON"}, status=400)
-
-    return JsonResponse({"error": "Invalid request"}, status=400)
+        payment, created = Payment.objects.update_or_create(
+            payment_id=payment_id,  
+            defaults={"status": payment_status},
+        )
+    return JsonResponse({"message": "DB update success"})
